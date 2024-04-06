3c7de993705ba77d31cfc0f7539ba3a5c9f1d4e9
Alexander Zibert <3660540+alexander-zibert@users.noreply.github.com>
2024-04-06 18:35:57 +0200

    add links

--- src/content/blog/snippets/01-fetch.mjs
+++ src/content/blog/snippets/01-fetch.mjs
@@ -1,3 +1,17 @@
-const response = await fetch("https://en.wikipedia.org/wiki/Jarrow_March").a();
+// start-0
+const response = await fetch("https://en.wikipedia.org/wiki/Jarrow_March");
 
-console.log(response);
+console.log(`${response.status} ${response.statusText}\n`);
+
+console.log("## Headers");
+for (const header of response.headers.entries()) {
+  console.log(header.join(": "));
+}
+// end-0
+// start-1
+console.log("\n## Body");
+let chunkIndex = 0;
+for await (const chunk of response.body) {
+  console.log(`Received chunk ${chunkIndex++} with ${chunk.length} bytes.`);
+}
+// end-1
